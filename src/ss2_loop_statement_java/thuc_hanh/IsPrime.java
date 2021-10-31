fe18b51532af498f20406d2c2dec1f85004045e7
C0921G1-quockhanh <ledinhquockhanhdn97@gmail.com>
2021-11-01 01:06:00 +0700

    upbai

--- src/ss2_loop_statement_java/thuc_hanh/IsPrime.java
+++ src/ss2_loop_statement_java/thuc_hanh/IsPrime.java
@@ -3,5 +3,10 @@ package ss2_loop_statement_java.thuc_hanh;
 import java.util.Scanner;
 
 public class IsPrime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+    }
 }
