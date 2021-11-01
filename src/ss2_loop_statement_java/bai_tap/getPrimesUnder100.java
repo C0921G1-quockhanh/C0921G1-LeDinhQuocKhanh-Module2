cbb22deb246d268a4e100bb6bffc5aa6c36c8eac
C0921G1-quockhanh <ledinhquockhanhdn97@gmail.com>
2021-11-01 23:06:48 +0700

    upbai

---
+++ src/ss2_loop_statement_java/bai_tap/getPrimesUnder100.java
@@ -0,0 +1,24 @@
+package ss2_loop_statement_java.bai_tap;
+
+public class getPrimesUnder100 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ src/ss2_loop_statement_java/bai_tap/getTwentyPrimes.java
@@ -0,0 +1,24 @@
+package ss2_loop_statement_java.bai_tap;
+
+public class getTwentyPrimes {
+    public static void main(String[] args) {
+        int numbers = 20;
+        int count = 0;
+        int N = 2;
+
+        while (count < numbers) {
+            boolean checkPrime = true;
+            for (int i = 2; i <= Math.sqrt(N); i++) {
+                if (N % i == 0) {
+                    checkPrime = false;
+                    break;
+                }
+            }
+            if (checkPrime) {
+                System.out.print(N + " ");
+                count += 1;
+            }
+            N += 1;
+        }
+    }
+}
