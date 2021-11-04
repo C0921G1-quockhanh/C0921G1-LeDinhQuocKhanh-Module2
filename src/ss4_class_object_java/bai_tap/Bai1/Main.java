95978d16a20a952f420f0e99c1b2c2bc1fe79833
C0921G1-quockhanh <ledinhquockhanhdn97@gmail.com>
2021-11-04 22:30:36 +0700

    upbai

---
+++ src/ss4_class_object_java/bai_tap/Bai1/Main.java
@@ -0,0 +1,4 @@
+package ss4_class_object_java.bai_tap.Bai1;
+
+public class Main {
+}
---
+++ src/ss4_class_object_java/bai_tap/Bai1/QuadraticEquation.java
@@ -0,0 +1,11 @@
+package ss4_class_object_java.bai_tap.Bai1;
+
+public class QuadraticEquation {
+    private double a,b,c;
+    QuadraticEquation(double inputA, double inputB, double inputC) {
+        this.a = inputA;
+        this.b = inputB;
+        this.c = inputC;
+    }
+
+}
